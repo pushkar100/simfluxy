123edf13a21fc74ea59c2e2b84f17fcc1523d166
Pushkar DK <pushkar@mysmartprice.com>
2019-04-22 01:15:29 +0530

    webpack changes to expose default export from o/p file in common js require() systems

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        libraryTarget: 'commonjs2',
+        libraryExport: 'default'
     },
     module: {
         rules: [{
